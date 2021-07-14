18199285f94a7cfd4ff00e43d7118d3a4d77b319
hammertime1308 <mandar1308@gmail.com>
2021-07-14 22:24:27 +0530

    web server version implementation

---
+++ server/server.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/hammertime1308/shape-sorter/shapes"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Error = ", err.Error())
+		os.Exit(2)
+	}
+	object := shapes.Parse(string(data))
+	object.Sort()
+	out := shapes.Serialize(object)
+	w.Write([]byte(out))
+}
+
+func Serve(port int) {
+	r := mux.NewRouter()
+
+	fmt.Println("Starting the server on port = ", port)
+
+	r.HandleFunc("/shape-sort", handler).Methods("GET")
+	http.ListenAndServe(fmt.Sprintf(":%v", port), r)
+}
